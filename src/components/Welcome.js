278d868856792b66e926fc13143f25b35d502eeb
adrianabelei <adrianabelei01@gmail.com>
2018-06-08 16:31:30 +0200

    medium

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,7 +8,7 @@ class Welcome extends Component {
             <h1 className="welcome-intro"><strong>Adriana Belei</strong> is about to start her career as a <strong>full stack web developer  </strong>&<strong> is living in Rotterdam. </strong>
             <br/>
             <br/>
-              <a href="https://www.linkedin.com/in/adrianabeleien/?locale=en_US" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin-square i-hov" aria-hidden="true"/></a>  <a href="" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium i.--hov" aria-hidden="true"/></a>  <a href="https://github.com/adrianabelei" target="_blank" rel="noopener noreferrer"><i className="fa fa-github i-hov" aria-hidden="true"/></a>  <a href="mailto:adrianabelei01@gmail.com?Subject=Hello%20there!" target="_top"><i className="fa fa-envelope i-hov" aria-hidden="true"/></a>
+              <a href="https://www.linkedin.com/in/adrianabeleien/?locale=en_US" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin-square i-hov" aria-hidden="true"/></a>  <a href="https://medium.com/@adrianabelei01" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium i.--hov" aria-hidden="true"/></a>  <a href="https://github.com/adrianabelei" target="_blank" rel="noopener noreferrer"><i className="fa fa-github i-hov" aria-hidden="true"/></a>  <a href="mailto:adrianabelei01@gmail.com?Subject=Hello%20there!" target="_top"><i className="fa fa-envelope i-hov" aria-hidden="true"/></a>
             <br/>
               
     
