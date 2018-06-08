33b201ff7c124dd3caf2f6193aee7e7120cfb728
adrianabelei <adrianabelei01@gmail.com>
2018-06-08 20:39:34 +0200

    medium

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ class Footer extends Component {
         <p><a href="mailto:adrianabelei01@gmail.com?Subject=Hello%20there!" target="_top"><i className="fa fa-envelope i-hov" aria-hidden="true"/> adrianabelei01@gmail.com</a></p>
           <p><a href="https://www.linkedin.com/in/adrianabeleien/?locale=en_US" target="_blank" rel="noopener noreferrer" className="footer-link"><i className="fa fa-linkedin-square i-hov" aria-hidden="true"/> Linkedin</a></p>
           <p><a href="https://github.com/adrianabelei" target="_blank" rel="noopener noreferrer"><i className="fa fa-github i-hov" aria-hidden="true"/> Github</a></p>
-          <p><a href="" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium i-hov" aria-hidden="true"/> Blog</a></p><br/>
+          <p><a href="https://medium.com/@adrianabelei01" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium i-hov" aria-hidden="true"/> Blog</a></p><br/>
           <h3>This Site</h3>
           <p>Made with ❤️ <strong>from Rotterdam</strong> using React, JavaScript and CSS</p>
       
